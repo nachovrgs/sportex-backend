b6441d0deb212a5977edc1ce8cabbfa5e80f8d5b
sapo <agustingc16@gmail.com>
2018-06-26 18:08:53 -0300

    Agregado de más DTOs.

---
+++ sportex.backoffice/Models/EventDTO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sportex.backoffice.Models
+{
+    public class EventDTO
+    {
+        #region PROPERTIES
+        public int ID { get; set; }
+        public int StandardProfileID { get; set; }
+        public StandardProfileDTO CreatorProfile { get; set; }
+        public string EventName { get; set; }
+        public string Description { get; set; }
+        public int EventType { get; set; }
+        public DateTime? StartingTime { get; set; }
+        public int LocationID { get; set; }
+        public LocationDTO Location { get; set; }
+        public bool IsPublic { get; set; }
+        public int MaxStarters { get; set; }
+        public int MaxSubs { get; set; }
+        public int CountStarters { get; set; }
+        public int CountSubs { get; set; }
+
+        #endregion
+
+        public EventDTO()
+        {
+            try
+            {
+                this.ID = 0;
+                this.StandardProfileID = 0;
+                this.CreatorProfile = null;
+                this.EventName = "";
+                this.Description = "";
+                this.EventType = 0;
+                this.StartingTime = DateTime.Now;
+                this.CountStarters = 0;
+                this.CountSubs = 0;
+                this.LocationID = 0;
+                this.Location = null;
+                this.IsPublic = false;
+                this.MaxStarters = 10;
+                this.MaxSubs = 10;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ sportex.backoffice/Models/LocationDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sportex.backoffice.Models
+{
+    public class LocationDTO
+    {
+        #region PROPERTIES
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? Latitude { get; set; }
+        public int? Longitude { get; set; }
+        #endregion
+
+        public LocationDTO()
+        {
+            try
+            {
+                this.ID = 0;
+                this.Name = "";
+                this.Description = "";
+                this.Latitude = null;
+                this.Longitude = null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
